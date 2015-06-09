817f685c2519a28b432f8655c198ae8e93aef5d3
Will Garrett <willgarrett610@gmail.com>
2015-06-08 20:50:52 -0600

    Copied files

---
+++ src/net/bobmandude9889/Infected/Main.java
@@ -0,0 +1,15 @@
+package net.bobmandude9889.Infected;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+	
+	public void onEnable(){
+		
+	}
+	
+	public void onDisable(){
+		
+	}
+	
+}
